e00a851e8e8652e663d41e73860e3aa1b779b814
vcpopa <81989171+vcpopa@users.noreply.github.com>
2022-11-10 13:53:54 +0000

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -1,5 +1,6 @@
 import nbformat as nbf
 import os
+import tempfile
 from pathlib import Path
 
 
@@ -12,5 +13,7 @@ class CodeGenerator():
     def make_notebook(self):
         nb = nbf.v4.new_notebook()
         nb['cells'] = [nbf.v4.new_code_cell(self.code_template)]
-        nbf.write(nb, f"./{self.scraper_name}.ipynb")
-        return f"./{self.scraper_name}.ipynb")
+        tmp = tempfile.NamedTemporaryFile
+        fn=f"{self.scraper_name}.ipynb"
+        with open(tmp.fn, 'w') as f:
+            nbf.write(nb, f)# where `stuff` is, y'know... stuff to write (a string)
