9f4b1c1c192cd0ec18a4b6e01d57b9f3a489caab
vcpopa <81989171+vcpopa@users.noreply.github.com>
2022-11-10 11:47:23 +0000

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -12,5 +12,5 @@ class CodeGenerator():
     def make_notebook(self):
         nb = nbf.v4.new_notebook()
         nb['cells'] = [nbf.v4.new_code_cell(self.code_template)]
-        download_path=r"{}".format(self.download_path)
-        nbf.write(nb, f'{os.path.join(download_path,self.scraper_name)}-Scraper.ipynb')
+        download_path=r"{}".format(self.download_path) +"\\\\"+self.scraper_name
+        nbf.write(nb, f'{download_path)}-Scraper.ipynb')
