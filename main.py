61889bde7c60a7d688b7c57c24b68123756c353e
vcpopa <81989171+vcpopa@users.noreply.github.com>
2022-11-10 13:56:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -97,8 +97,7 @@ def main():
             codegen=CodeGenerator(download_path=download_path,scraper_name=scraper_name,code_template=template)
             nb=codegen.make_notebook()
             st.download_button("Download bot notebook",data=nb,file_name=f"{scraper_name}.ipynb")
-            
-#             st.markdown(f"{scraper_name} bot notebook created successfully @ {download_path}")
+           
         except:
             st.markdown(traceback.format_exc())
 
